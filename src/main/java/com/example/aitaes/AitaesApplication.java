3a0f81dab8b7de4cd60ccf0fbfe42f9a8e10fdd5
zwq <2907551361@qq.com>
2026-07-07 14:47:55 +0800

    commit

---
+++ src/main/java/com/example/aitaes/AitaesApplication.java
@@ -0,0 +1,13 @@
+package com.example.aitaes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitaesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AitaesApplication.class, args);
+    }
+
+}
